ab1ee8fa1e2c56e154089491331081feb43868b6
Igor Zubkov <igor.zubkov@gmail.com>
2017-01-24 17:16:12 +0200

    Api::V1::SignInsController

---
+++ app/controllers/api/v1/sign_ins_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class SignInsController < BaseController
+      skip_before_action :authenticate
+
+      private
+
+      def build_resource
+        @sign_in = Api::SignIn.new(resource_params)
+      end
+
+      def resource
+        @sign_in
+      end
+
+      def resource_params
+        params.permit(:email, :password)
+      end
+    end
+  end
+end
---
+++ spec/controllers/api/v1/sign_ins_controller_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+describe Api::V1::SignInsController do
+  it { should be_a(Api::V1::BaseController) }
+
+  it { should_not use_before_action(:authenticate) }
+
+  describe '#create.json' do
+    context 'successful authorization' do
+      let!(:user) { create(:user, password: 'password') }
+
+      before do
+        post :create, params: { email: user.email, password: 'password', format: :json }
+      end
+
+      it { should render_template(:create) }
+
+      it { should respond_with(:ok) }
+    end
+
+    context 'failed authorization' do
+      before do
+        post :create, params: { email: 'me@example.com', password: 'password', format: :json }
+      end
+
+      it { should render_template(:errors) }
+
+      it { should respond_with(:unprocessable_entity) }
+    end
+  end
+
+  # private methods
+
+  describe '#build_resource' do
+    let(:resource_params) { double }
+
+    let(:sign_in) { double }
+
+    before { expect(subject).to receive(:resource_params).and_return(resource_params) }
+
+    before { expect(Api::SignIn).to receive(:new).with(resource_params).and_return(sign_in) }
+
+    specify { expect { subject.send(:build_resource) }.not_to raise_error }
+
+    specify { expect { subject.send(:build_resource) }.to change { subject.instance_variable_get(:@sign_in) }.from(nil).to(sign_in) }
+  end
+
+  describe '#resource' do
+    let(:sign_in) { double }
+
+    before { subject.instance_variable_set(:@sign_in, sign_in) }
+
+    specify { expect(subject.send(:resource)).to eq(sign_in) }
+  end
+
+  describe '#resource_params' do
+    before do
+      #
+      # subject.params.permit(:email, :password)
+      #
+      expect(subject).to receive(:params) do
+        double.tap do |a|
+          expect(a).to receive(:permit).with(:email, :password)
+        end
+      end
+    end
+
+    specify { expect { subject.send(:resource_params) }.not_to raise_error }
+  end
+end
