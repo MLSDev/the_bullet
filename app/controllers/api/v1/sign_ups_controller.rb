b25dd50786391790f4fda9c36614c58c33a20108
Igor Zubkov <igor.zubkov@gmail.com>
2017-01-24 17:09:42 +0200

    Api::V1::SignUpsController

---
+++ app/controllers/api/v1/sign_ups_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class SignUpsController < BaseController
+      skip_before_action :authenticate
+
+      private
+
+      def build_resource
+        @sign_up ||= Api::SignUp.new(resource_params)
+      end
+
+      def resource
+        @sign_up
+      end
+
+      def resource_params
+        params.permit(:email, :password, :password_confirmation)
+      end
+    end
+  end
+end
