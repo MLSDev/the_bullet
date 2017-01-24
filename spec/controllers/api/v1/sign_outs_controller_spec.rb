f792b33fd590a9cd37ee672d96e1819a50465383
Igor Zubkov <igor.zubkov@gmail.com>
2017-01-24 17:12:22 +0200

    Api::V1::SignOutsController

---
+++ spec/controllers/api/v1/sign_outs_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe Api::V1::SignOutsController do
+  it { should be_a(Api::V1::BaseController) }
+
+  it { should use_before_action(:authenticate) }
+
+  describe '#destroy.json' do
+    context 'authorized' do
+      let!(:user) { create(:user) }
+
+      let!(:session) { create(:session, user: user) }
+
+      before { request.env['HTTP_AUTHORIZATION'] = "Bearer #{ session.token }" }
+
+      before { delete :destroy, format: :json }
+
+      it { should respond_with(:ok) }
+    end
+
+    context 'not authorized' do
+      before { delete :destroy, format: :json }
+
+      it { should respond_with(:unauthorized) }
+    end
+  end
+end
