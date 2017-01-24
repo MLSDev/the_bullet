643b54347f65bc8da5c00cc78816704ba2b086dc
Igor Zubkov <igor.zubkov@gmail.com>
2017-01-24 15:21:12 +0200

    User has many sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :sessions, dependent: :destroy
+
   validates :email, presence: true
 
   validates :email, uniqueness: { case_sensitive: false }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe User do
+  it { should have_many(:sessions).dependent(:destroy) }
+
   it { should validate_presence_of(:email) }
 
   it { should validate_uniqueness_of(:email).case_insensitive }
