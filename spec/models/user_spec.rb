00fd75959833696a3c8968a9cbc4edece4677f41
Igor Zubkov <igor.zubkov@gmail.com>
2017-01-24 17:01:11 +0200

    More specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe User do
+  it { should be_a(ApplicationRecord) }
+
   it { should have_many(:sessions).dependent(:destroy) }
 
   it { should validate_presence_of(:email) }
