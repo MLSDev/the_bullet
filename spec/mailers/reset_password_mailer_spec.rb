347b25ee4119b12db1e8602e28b05d8d36a4401f
Igor Zubkov <igor.zubkov@gmail.com>
2017-01-30 17:56:27 +0200

    More specs

--- spec/mailers/reset_password_mailer_spec.rb
+++ spec/mailers/reset_password_mailer_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 describe ResetPasswordMailer do
+  describe '#email' do
+    let(:user) { create(:user) }
+
+    let(:mail) { described_class.email(user.id) }
+
+    specify { expect(mail.subject).to eq('Reset password mail') }
+
+    specify { expect(mail.to).to eq([user.email]) }
+
+    specify { expect(mail.from).to eq(['support@example.com']) }
+
+    specify { expect(mail.body.encoded).to include(user.reset_token) }
+  end
 end
