167135669f406f4dc23c8eaf3e35aec67da7cf1f
Igor Zubkov <igor.zubkov@gmail.com>
2017-02-20 18:01:31 +0200

    Backoffice::SessionDecorator

---
+++ app/decorators/backoffice/session_decorator.rb
@@ -0,0 +1,24 @@
+module Backoffice
+  class SessionDecorator < Draper::Decorator
+    delegate_all
+
+    def as_json(*)
+      {
+        id: id,
+        token: token,
+        created_at: created_at,
+        updated_at: updated_at
+      }
+    end
+
+    private
+
+    def created_at
+      model.created_at.iso8601
+    end
+
+    def updated_at
+      model.updated_at.iso8601
+    end
+  end
+end
