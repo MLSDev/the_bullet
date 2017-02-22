2222c09c864f3e3f62ba54484d809c7a4d14132f
Igor Zubkov <igor.zubkov@gmail.com>
2017-02-22 12:43:03 +0200

    Backoffice::UserDecorator

---
+++ spec/decorators/backoffice/user_decorator_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Backoffice::UserDecorator do
+  describe '#as_json' do
+    let!(:user) { create(:user) }
+
+    subject { user.decorate.as_json }
+
+    its([:id]) { should eq(user.id) }
+
+    its([:email]) { should eq(user.email) }
+
+    its([:created_at]) { should eq(user.created_at.iso8601) }
+
+    its([:updated_at]) { should eq(user.updated_at.iso8601) }
+  end
+end
