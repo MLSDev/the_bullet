80316e3a76b300e3115ed15062b33546c3eac136
Igor Zubkov <igor.zubkov@gmail.com>
2017-02-23 16:43:17 +0200

    Backoffice::Docs::Models::OutputUsersCollection

---
+++ lib/backoffice/docs/models/output_users_collection.rb
@@ -0,0 +1,12 @@
+module Backoffice
+  module Docs
+    module Models
+      class OutputUsersCollection
+        # :nocov:
+        include Swagger::Blocks
+
+        # :nocov:
+      end
+    end
+  end
+end
