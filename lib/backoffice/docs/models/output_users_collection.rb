557d356d2349c607baddb903e4d9422c07423106
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-02 16:41:07 +0200

    Update backoffice swagger

--- lib/backoffice/docs/models/output_users_collection.rb
+++ lib/backoffice/docs/models/output_users_collection.rb
@@ -5,6 +5,45 @@ module Backoffice
         # :nocov:
         include Swagger::Blocks
 
+        swagger_schema :OutputUsersCollection do
+          key :required, [:collection, :total_pages, :current_page, :total_count, :per_page]
+          property :collection do
+            key :type, :array
+            items do
+              key :'$ref', :OutputUser
+            end
+          end
+          property :total_pages do
+            key :type, :integer
+            key :format, :int64
+            key :description, 'Total pages count'
+          end
+          property :current_page do
+            key :type, :integer
+            key :format, :int64
+            key :description, 'Current page number'
+          end
+          property :total_count do
+            key :type, :integer
+            key :format, :int64
+            key :description, 'Total users count'
+          end
+          property :per_page do
+            key :type, :integer
+            key :format, :int64
+            key :description, 'Per page value'
+          end
+          key :example, total_page: 4,
+                        current_page: 1,
+                        total_count: 100,
+                        per_page: 25
+
+          # key :example, collection: {},
+          #               total_page: 4,
+          #               current_page: 1,
+          #               total_count: 100,
+          #               per_page: 25
+        end
         # :nocov:
       end
     end
