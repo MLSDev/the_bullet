b5d3a7e3114ecf0e462ac504d3a056a40fa14ae3
Igor Zubkov <igor.zubkov@gmail.com>
2017-05-31 10:43:24 +0300

    Cleanup

--- config/initializers/rack-cors.rb
+++ config/initializers/rack-cors.rb
@@ -16,4 +16,3 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
-# rubocop:enable Style/FileName
