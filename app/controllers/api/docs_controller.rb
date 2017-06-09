868675be24cacf7077a59101dfcc766d1f13f2d9
Igor Zubkov <igor.zubkov@gmail.com>
2017-06-09 13:48:35 +0300

    Protect from forgery with exception

--- app/controllers/api/docs_controller.rb
+++ app/controllers/api/docs_controller.rb
@@ -1,5 +1,7 @@
 module Api
   class DocsController < ActionController::Base
+    protect_from_forgery with: :exception
+
     include Swagger::Blocks
 
     # :nocov:
--- app/controllers/backoffice/docs_controller.rb
+++ app/controllers/backoffice/docs_controller.rb
@@ -1,5 +1,7 @@
 module Backoffice
   class DocsController < ActionController::Base
+    protect_from_forgery with: :exception
+
     include Swagger::Blocks
 
     # :nocov:
