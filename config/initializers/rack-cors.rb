b7d674b5760f617f6c24431132b96e9d48ab6ad1
Igor Zubkov <igor.zubkov@gmail.com>
2017-11-23 16:32:20 +0200

    Fix rubocop warnings

--- config/initializers/rack-cors.rb
+++ config/initializers/rack-cors.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# rubocop:disable Style/FileName
-
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
